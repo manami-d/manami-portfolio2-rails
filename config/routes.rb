a7a4df93446a8ed98e397723a53f67ad6e2a300e
manami-d <manami.davidson@hotmail.com>
2021-04-20 11:38:12 +1000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'projects/index'
+  get 'projects/show'
+  get 'projects/contact'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
